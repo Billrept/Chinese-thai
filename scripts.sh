813b635bd449ee0440beb89fd7d54672f8ffaf56
BillRAI <150030700+BillRAI@users.noreply.github.com>
2025-06-09 18:06:59 +0800

    fix script

--- scripts.sh
+++ scripts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -p compute                     # specify GPU partition
+#SBATCH -p gpu                     # specify GPU partition
 #SBATCH -N 1                       # number of nodes
 #SBATCH --ntasks-per-node=1        # number of tasks per node
 #SBATCH -t 24:00:00                # job time limit <hr:min:sec>
@@ -13,4 +13,4 @@ module load Mamba/23.11.0-0
 conda activate 120_wangmak
 
 # Run the fine-tuning script
-srun python finetune.py
+srun python finetune.py 
