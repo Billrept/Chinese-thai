7d9cbc32f9dd5860b925fbe89182223578614a94
BillRAI <150030700+BillRAI@users.noreply.github.com>
2025-06-12 13:39:37 +0800

    update memory alloc

--- scripts.sh
+++ scripts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -p gpu                     # specify GPU partition
 #SBATCH -N 1                       # number of nodes
-#SBATCH --ntasks-per-node=4        # number of tasks per node
+#SBATCH --ntasks-per-node=2       # number of tasks per node
 #SBATCH --gpus-per-task=4          # request 4 GPUs
 #SBATCH -t 24:00:00                # job time limit <hr:min:sec>
 #SBATCH -A tb901117                # specify your account ID
